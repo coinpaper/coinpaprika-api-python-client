4ae2dfe3e9fa0d7001a6b685636916872b35dab1
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2021-01-20 16:15:43 +0100

    Removed main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src import CoinpaprikaClient
-from datetime import datetime, timedelta
-
-if __name__ == "__main__":
-    api_client = CoinpaprikaClient()
-    binance_markets = api_client.exchanges.markets("binance")
-
-    print(binance_markets)
